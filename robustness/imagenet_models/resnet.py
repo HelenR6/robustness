f30347da0a2e6772f35f5775bc018a7807607d65
Helen_Ren <59241451+HelenR6@users.noreply.github.com>
2021-11-22 21:24:41 -0500

    Update resnet.py

--- robustness/imagenet_models/resnet.py
+++ robustness/imagenet_models/resnet.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
-from torch.hub import load_state_dict_from_url
+# from torch.hub import load_state_dict_from_url
+from torch.utils.model_zoo import load_url as load_state_dict_from_url
 from ..tools.custom_modules import SequentialWithArgs, FakeReLU
 
 
