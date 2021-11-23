68072dc38ace6402478ea4e2be62efaeb2dfd861
Helen_Ren <59241451+HelenR6@users.noreply.github.com>
2021-11-22 21:25:21 -0500

    Update alexnet.py

--- robustness/imagenet_models/alexnet.py
+++ robustness/imagenet_models/alexnet.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
-from torchvision.models.utils import load_state_dict_from_url
+# from torchvision.models.utils import load_state_dict_from_url
+from torch.utils.model_zoo import load_url as load_state_dict_from_url
 from ..tools.custom_modules import FakeReLUM
 
 __all__ = ['AlexNet', 'alexnet']
